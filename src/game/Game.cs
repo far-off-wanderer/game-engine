5d2e6e15685902b6cb8b718acc3c150199a2b533
David Spörri <davepermen@hotmail.com>
2017-03-12 17:35:46 +0100

    added 3d model, properly renders (when saved as .obj)

--- src/game/Game.cs
+++ src/game/Game.cs
@@ -10,9 +10,15 @@
         SpriteBatch spriteBatch;
         Texture2D texture;
 
+        Model model;
+
+        float angle;
+
         public Game()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = true;
+            IsMouseVisible = false;
             Content.RootDirectory = "Content";
             Window.AllowUserResizing = true;
         }
@@ -26,6 +32,8 @@
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             texture = Content.Load<Texture2D>("conesoft entertainment presents");
+            model = Content.Load<Model>("ships/main");
+            angle = 0f;
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,16 +44,38 @@
             {
                 Exit();
             }
+            var speed = 0.25;
+            angle = (float)(speed * gameTime.TotalGameTime.TotalSeconds * 360) % 360;
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+ 
             spriteBatch.Begin(samplerState: SamplerState.PointClamp);
             spriteBatch.Draw(texture, Window.ClientBounds.Fill(texture.Width, texture.Height), Color.White);
             spriteBatch.End();
+
+            spriteBatch.Begin(samplerState: SamplerState.AnisotropicClamp);
+            spriteBatch.End();
+
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+            model.Draw(
+                Matrix.CreateRotationY(MathHelper.ToRadians(angle)) * Matrix.CreateTranslation(25f, -12.5f, 0),
+                Matrix.CreateLookAt(
+                    Vector3.Backward * 50,
+                    Vector3.Zero,
+                    Vector3.Up
+                ),
+                Matrix.CreatePerspectiveFieldOfView(
+                    MathHelper.ToRadians(60),
+                    GraphicsDevice.DisplayMode.AspectRatio,
+                    1f,
+                    1000f
+                )
+            );
 
             base.Draw(gameTime);
         }
