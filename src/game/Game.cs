a2b3a795e9fb698a22bb9337d75ea24579f3c6f5
David Spörri <davepermen@hotmail.com>
2017-03-22 00:35:17 +0100

    first shader

--- src/game/Game.cs
+++ src/game/Game.cs
@@ -4,17 +4,38 @@
     using Microsoft.Xna.Framework.Graphics;
     using Microsoft.Xna.Framework.Input;
 
+    class ModelWithTexture
+    {
+        public Model Model { get; private set; }
+        public Texture2D Texture { get; private set; }
+
+        public ModelWithTexture(Model model)
+        {
+            this.Model = model;
+            this.Texture = ((BasicEffect)model.Meshes[0].Effects[0]).Texture;
+        }
+
+
+        public static implicit operator ModelWithTexture(Model model)
+        {
+            return new ModelWithTexture(model);
+        }
+    }
+
     public class Game : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D conesoft_entertainment_presents;
         Texture2D far_off_wanderer;
+        Effect main;
 
-        Model model;
+        ModelWithTexture ship;
+
 
         float angle;
         float title_fadein;
+        float brightness;
 
         public Game()
         {
@@ -38,7 +59,8 @@
             spriteBatch = new SpriteBatch(GraphicsDevice);
             conesoft_entertainment_presents = Content.Load<Texture2D>("conesoft entertainment presents");
             far_off_wanderer = Content.Load<Texture2D>("far off wanderer");
-            model = Content.Load<Model>("ships/main");
+            ship = Content.Load<Model>("ships/main");
+            main = Content.Load<Effect>("main");
             angle = 0f;
         }
 
@@ -52,6 +74,8 @@
             }
             var speed = 0.25;
             angle = (float)(speed * gameTime.TotalGameTime.TotalSeconds * 360) % 360;
+
+            brightness = (float)gameTime.TotalGameTime.TotalSeconds;
 
             title_fadein = MathHelper.SmoothStep(0, 1, MathHelper.Clamp((float)gameTime.TotalGameTime.TotalSeconds - 5, 0, 1));
             base.Update(gameTime);
@@ -80,20 +104,32 @@
                 Matrix.CreateTranslation(25f, -12.5f, 0)
                 ;
 
-            model.Draw(
-                Matrix.CreateRotationY(MathHelper.ToRadians(angle)) * translation,
-                Matrix.CreateLookAt(
+            var world = Matrix.CreateRotationY(MathHelper.ToRadians(angle)) * translation;
+            var view = Matrix.CreateLookAt(
                     Vector3.Backward * 50,
                     Vector3.Zero,
                     Vector3.Up
-                ),
-                Matrix.CreatePerspectiveFieldOfView(
+                );
+            var projection = Matrix.CreatePerspectiveFieldOfView(
                     MathHelper.ToRadians(60),
                     aspectRatio,
                     1f,
                     1000f
-                )
-            );
+                );
+
+            foreach (ModelMesh mesh in ship.Model.Meshes)
+            {
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+                    part.Effect = main;
+                    var worldViewProjection = (world * mesh.ParentBone.Transform) * view * projection;
+                    main.Parameters["worldViewProjection"].SetValue(worldViewProjection);
+                    main.Parameters["worldViewProjectionTransposed"].SetValue(Matrix.Transpose(worldViewProjection));
+                    main.Parameters["modelTexture"].SetValue(ship.Texture);
+                    main.Parameters["brightness"].SetValue(brightness);
+                }
+                mesh.Draw();
+            }
 
             /* title */
             spriteBatch.Begin(samplerState: SamplerState.PointClamp, blendState: BlendState.Additive);
